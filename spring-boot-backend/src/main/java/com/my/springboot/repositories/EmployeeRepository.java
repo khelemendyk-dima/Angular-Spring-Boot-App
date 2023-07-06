9aa006389a96edba43473ffda8903c0edfbecf62
Dima Khelemendyk <khelemendykdmytro@gmail.com>
2023-07-06 15:47:47 +0300

    initial commit

---
+++ spring-boot-backend/src/main/java/com/my/springboot/repositories/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.my.springboot.repositories;
+
+import com.my.springboot.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
